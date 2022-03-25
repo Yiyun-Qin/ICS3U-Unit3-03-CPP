5951df801372b4d37f93e484f3a74613d9e47aa7
Yiyun Qin <yiyun.qin@mths.ca>
2022-03-25 18:29:39 +0000

    random number guess

--- numberGuess.cpp
+++ numberGuess.cpp
@@ -5,24 +5,30 @@
 // This is a math program, calculating the number guess game
 
 #include <iostream>
+#include <random>
 
 int main() {
     // This function comparing the guessed number and answer
-    const int ANSWER = 6;
     int numberGuess;
+    int answerRandom;
 
     // input
     std::cout << "Enter the number you guess between 0 - 9: ";
     std::cin >> numberGuess;
 
     // process
-    std::cout << std::endl;
-    if (numberGuess == ANSWER) {
+    std::random_device rseed;
+    std::mt19937 rgen(rseed());
+    std::uniform_int_distribution<int> idist(0, 9);
+    answerRandom = idist(rgen);
+
+    std::cout << "" << std::endl;
+    if (numberGuess == answerRandom) {
         // output
         std::cout << "Your guess is right!";
-    }
-    if (numberGuess != ANSWER) {
-        std::cout << "Your guess is wrong!";
+    } else {
+        std::cout << "Your guess is wrong! The answer is "
+        << answerRandom << "." << std::endl;
     }
     std::cout << "\nDone." << std::endl;
 }
